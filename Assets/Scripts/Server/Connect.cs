2601b7576063cbd4ab51e2d88148ab1c2672b0bc
jjhlzn <jinjunhang@hotmail.com>
2017-10-28 21:30:59 +0800

    add socket.io client dependency
    https://github.com/nhnent/socket.io-client-unity3d

---
+++ Assets/Scripts/Server/Connect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using socket.io;
+
+public class Connect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		var serverUrl = "http://localhost:3000";
+		var socket = Socket.Connect(serverUrl);
+
+		socket.On(SystemEvents.connect, () => {
+			Debug.Log("Hello, Socket.io~");
+		});
+
+		socket.On(SystemEvents.reconnect, (int reconnectAttempt) => {
+			Debug.Log("Hello, Again! " + reconnectAttempt);
+		});
+
+		socket.On(SystemEvents.disconnect, () => {
+			Debug.Log("Bye~");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
