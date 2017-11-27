5b4ae5d021ef9d9c8ffd664c651d18fed2988a6c
jjhlzn <jinjunhang@hotmail.com>
2017-11-27 11:59:55 +0800

    可以在iOS中运行（但是在模拟器中，画面非常卡顿）

--- Assets/Scripts/Server/ServerUtils.cs
+++ Assets/Scripts/Server/ServerUtils.cs
@@ -7,7 +7,7 @@ public class ServerUtils
 	{
 	}
 
-	public static string mainServer =   "192.168.31.175"; //"localhost" ; 
+	public static string mainServer =   "192.168.1.114"; // "192.168.31.175"; //"localhost" ;  //"192.168.1.114";
 	public static string protocol = "http";
 	public static int port = 3000;
 
